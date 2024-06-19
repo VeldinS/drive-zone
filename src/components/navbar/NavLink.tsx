cbaec6cdf52d56bbdae284e119606ba873cee910
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-19 19:24:49 +0200

    Navbar implementation - left side - dropdown update

--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -3,18 +3,24 @@ import Link from "next/link";
 import Image from "next/image";
 
 import downArrow from '@/assets/icons/down arrow.svg';
-import rightArrow from '@/assets/icons/right arrow.svg';
+import {StaticImport} from "next/dist/shared/lib/get-img-props";
 
 interface navLinkProps {
     dropdown: boolean,
     to?: string,
     name: string,
     dropdownItem1?: string,
+    dropdownItem1Icon?: StaticImport | string,
     dropdownItem2?: string,
+    dropdownItem2Icon?: StaticImport | string,
     dropdownItem3?: string,
+    dropdownItem3Icon?: StaticImport | string,
     dropdownItem4?: string,
+    dropdownItem4Icon?: StaticImport | string,
     dropdownItem5?: string,
+    dropdownItem5Icon?: StaticImport | string,
     dropdownItem6?: string,
+    dropdownItem6Icon?: StaticImport | string,
 }
 
 function NavLink(navlinkProps: navLinkProps) {
@@ -35,43 +41,55 @@ function NavLink(navlinkProps: navLinkProps) {
                         <p className="font-audiowide font-medium text-base text-[#050B20]">
                             {navlinkProps.name}
                         </p>
-                        <Image src={downArrow} alt="Arrow icon." width={20} />
+                        <Image src={downArrow} alt="Arrow icon." width={20} className={'transition duration-500 rotate-0 group-hover:rotate-180'}/>
                     </div>
-                    <div className="absolute flex top-full left-0 bg-[#E9F2FF] flex-col justify-start items-start gap-4 px-4 pb-6 pt-4 rounded-2xl rounded-l-[0] shadow-lg
-                                    transition-all duration-500 opacity-0 group-hover:opacity-100 w-0 group-hover:w-[150px] ">
+                    <div className="absolute flex top-full left-0 bg-[#E9F2FF] flex-col justify-start items-start gap-6 px-4 pb-6 pt-4 rounded-2xl rounded-l-[0] shadow-lg
+                                    transition-all duration-500 opacity-0 group-hover:opacity-100 w-0 group-hover:w-[200px] ">
                         {navlinkProps.dropdownItem1 && (
-                            <Link href={`${navlinkProps.dropdownItem1}`} className="flex flex-row justify-start items-center gap-2">
-                                <Image src={rightArrow} alt="Dropdown menu icon." width={20} />
+                            <Link href={`${navlinkProps.dropdownItem1}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                                {navlinkProps.dropdownItem1Icon && (
+                                    <Image src={navlinkProps.dropdownItem1Icon} alt="Dropdown menu icon." width={30} />
+                                )}
                                 <p className="font-audiowide font-medium text-base text-[#050B20] text-nowrap">{navlinkProps.dropdownItem1}</p>
                             </Link>
                         )}
                         {navlinkProps.dropdownItem2 && (
-                            <Link href={`${navlinkProps.dropdownItem2}`} className="flex flex-row justify-start items-center gap-2">
-                                <Image src={rightArrow} alt="Dropdown menu icon." width={20} />
+                            <Link href={`${navlinkProps.dropdownItem2}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                                {navlinkProps.dropdownItem2Icon && (
+                                    <Image src={navlinkProps.dropdownItem2Icon} alt="Dropdown menu icon." width={30} />
+                                )}
                                 <p className="font-audiowide font-medium text-base text-[#050B20] text-nowrap">{navlinkProps.dropdownItem2}</p>
                             </Link>
                         )}
                         {navlinkProps.dropdownItem3 && (
-                            <Link href={`${navlinkProps.dropdownItem3}`} className="flex flex-row justify-start items-center gap-2">
-                                <Image src={rightArrow} alt="Dropdown menu icon." width={20} />
+                            <Link href={`${navlinkProps.dropdownItem3}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                                {navlinkProps.dropdownItem3Icon && (
+                                    <Image src={navlinkProps.dropdownItem3Icon} alt="Dropdown menu icon." width={30} />
+                                )}
                                 <p className="font-audiowide font-medium text-base text-[#050B20] text-nowrap">{navlinkProps.dropdownItem3}</p>
                             </Link>
                         )}
                         {navlinkProps.dropdownItem4 && (
-                            <Link href={`${navlinkProps.dropdownItem4}`} className="flex flex-row justify-start items-center gap-2">
-                                <Image src={rightArrow} alt="Dropdown menu icon." width={20} />
+                            <Link href={`${navlinkProps.dropdownItem4}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                                {navlinkProps.dropdownItem4Icon && (
+                                    <Image src={navlinkProps.dropdownItem4Icon} alt="Dropdown menu icon." width={30} />
+                                )}
                                 <p className="font-audiowide font-medium text-base text-[#050B20] text-nowrap">{navlinkProps.dropdownItem4}</p>
                             </Link>
                         )}
                         {navlinkProps.dropdownItem5 && (
-                            <Link href={`${navlinkProps.dropdownItem5}`} className="flex flex-row justify-start items-center gap-2">
-                                <Image src={rightArrow} alt="Dropdown menu icon." width={20} />
+                            <Link href={`${navlinkProps.dropdownItem5}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                                {navlinkProps.dropdownItem5Icon && (
+                                    <Image src={navlinkProps.dropdownItem5Icon} alt="Dropdown menu icon." width={30} />
+                                )}
                                 <p className="font-audiowide font-medium text-base text-[#050B20] text-nowrap">{navlinkProps.dropdownItem5}</p>
                             </Link>
                         )}
                         {navlinkProps.dropdownItem6 && (
-                            <Link href={`${navlinkProps.dropdownItem6}`} className="flex flex-row justify-start items-center gap-2">
-                                <Image src={rightArrow} alt="Dropdown menu icon." width={20} />
+                            <Link href={`${navlinkProps.dropdownItem6}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                                {navlinkProps.dropdownItem6Icon && (
+                                    <Image src={navlinkProps.dropdownItem6Icon} alt="Dropdown menu icon." width={30} />
+                                )}
                                 <p className="font-audiowide font-medium text-base text-[#050B20] text-nowrap">{navlinkProps.dropdownItem6}</p>
                             </Link>
                         )}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,16 +2,44 @@ import React from 'react';
 
 import Image from "next/image";
 
-import logo from '@/assets/logo/logo.png';
 import NavLink from "@/components/navbar/NavLink";
+
+import logo from '@/assets/logo/logo.png';
+
+import volkswagen from '@/assets/brands/volkswagen.svg';
+import audi from '@/assets/brands/audi.svg';
+import toyota from '@/assets/brands/toyota.svg';
+import bmw from '@/assets/brands/bmw.svg';
+import volvo from '@/assets/brands/volvo.svg';
+import mercedes from '@/assets/brands/mercedes.svg';
+
+import about from '@/assets/icons/about.svg';
+import sell from '@/assets/icons/sell-car.svg';
+import buy from '@/assets/icons/buy-car.svg';
+import popular from '@/assets/icons/popular.svg';
+import contact from '@/assets/icons/contact.svg';
+
 
 function Navbar() {
     return (
         <nav className={'w-full h-auto py-4 px-[10%] flex flex-row justify-between items-center bg-transparent'}>
             <div className={'flex flex-row justify-start items-center gap-6'}>
                 <Image src={logo} alt="Logo image." width="150" />
-                <NavLink dropdown={true} name={'Home'} dropdownItem1={'About Us'} dropdownItem2={'Sell Car'} dropdownItem3={'Buy Car'} dropdownItem4={'Selected'} dropdownItem5={'Contact Us'}/>
-                <NavLink dropdown={true} name={'Brands'} dropdownItem1={'Volkswagen'} dropdownItem2={'Audi'} dropdownItem3={'Toyota'} dropdownItem4={'BMW'} dropdownItem5={'Volvo'} dropdownItem6={'Mercedes'} />
+                <NavLink dropdown={true} name={'Home'}
+                         dropdownItem1={'About Us'} dropdownItem1Icon={about}
+                         dropdownItem2={'Sell Car'} dropdownItem2Icon={sell}
+                         dropdownItem3={'Buy Car'} dropdownItem3Icon={buy}
+                         dropdownItem4={'Popular'} dropdownItem4Icon={popular}
+                         dropdownItem5={'Contact Us'} dropdownItem5Icon={contact}
+                />
+                <NavLink dropdown={true} name={'Brands'}
+                         dropdownItem1={'Volkswagen'} dropdownItem1Icon={volkswagen}
+                         dropdownItem2={'Audi'} dropdownItem2Icon={audi}
+                         dropdownItem3={'Toyota'} dropdownItem3Icon={toyota}
+                         dropdownItem4={'BMW'} dropdownItem4Icon={bmw}
+                         dropdownItem5={'Volvo'} dropdownItem5Icon={volvo}
+                         dropdownItem6={'Mercedes'} dropdownItem6Icon={mercedes}
+                />
                 <NavLink dropdown={true} name={'Types'} dropdownItem1={'SUV'} dropdownItem2={'Sedan'} dropdownItem3={'Hatchback'} dropdownItem4={'Coupe'} dropdownItem5={'Hybrid'} />
                 <NavLink dropdown={false} to={'/cars'} name={'All Cars'} />
                 <NavLink dropdown={false} to={'/about'} name={'About'} />
