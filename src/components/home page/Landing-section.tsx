7d1f534944888fe9193d4ecf20bbe0f683c4e7e3
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-20 13:06:21 +0200

    Landing section - desktop version complete

--- src/components/home page/Landing-section.tsx
+++ src/components/home page/Landing-section.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 import '@/app/globals.css'
 import car from '@/assets/content images/car-landing.jpg';
 import Image from "next/image";
+import ButtonWhite from "@/components/buttons/ButtonWhite";
+import ButtonBlue from "@/components/buttons/ButtonBlue";
+import ModelButton from "@/components/buttons/ModelButton";
+
+import suv from '@/assets/car types/suv.svg';
+import sedan from '@/assets/car types/sedan.svg';
+import hatchback from '@/assets/car types/hatchbag.svg';
+import hybrid from '@/assets/car types/hybrid.svg';
+import coupe from '@/assets/car types/coupe.svg';
+import Link from "next/link";
 
 function LandingSection() {
     return (
         <section className={'w-screen h-screen flex flex-row justify-start items-start'}>
-            <div className={'w-2/3 h-full pl-[5%] flex flex-col justify-start items-start'}>
-
+            <div className={'w-2/3 h-full pl-[5%] flex flex-col justify-center items-start gap-8'}>
+                <p className={'font-poppins text-black font-normal text-base'}>Find cars for sale and for rent near you</p>
+                <h1 className={'font-poppins text-black font-medium text-7xl leading-normal'}>4,675 Vehicles <br/> Available</h1>
+                <div className={'flex flex-row justify-start items-start gap-8'}>
+                    <ButtonBlue text={'Views Inventory'} link={'/cars'} />
+                    <ButtonWhite text={'Contact Us'} link={'/contact'} />
+                </div>
+                <div className={'flex flex-col justify-start items-start gap-6'}>
+                    <p className={'font-poppins text-black font-normal text-base'}>Or Browse Featured Models</p>
+                    <div className={'flex flex-row justify-start items-start gap-4'}>
+                        <ModelButton modelIcon={suv} modelName={'SUV'} modelLink={'/suv'} />
+                        <ModelButton modelIcon={sedan} modelName={'Sedan'} modelLink={'/sedan'} />
+                        <ModelButton modelIcon={hatchback} modelName={'Hatchback'} modelLink={'/hatchback'} />
+                        <ModelButton modelIcon={coupe} modelName={'Coupe'} modelLink={'/coupe'} />
+                        <ModelButton modelIcon={hybrid} modelName={'Hybrid'} modelLink={'/hybrid'} />
+                    </div>
+                </div>
             </div>
             <div className={'relative w-1/3 h-full landing-image'}>
                 <Image src={car} alt={'Car image'}
--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -46,7 +46,7 @@ function NavLink(navlinkProps: navLinkProps) {
                     <div className="absolute flex top-full left-0 bg-[#E9F2FF] flex-col justify-start items-start gap-6 px-4 pb-6 pt-4 rounded-2xl rounded-l-[0] shadow-lg
                                     transition-all duration-500 opacity-0 group-hover:opacity-100 w-0 group-hover:w-[200px] ">
                         {navlinkProps.dropdownItem1 && (
-                            <Link href={`${navlinkProps.dropdownItem1}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                            <Link href={`${navlinkProps.dropdownItem1}`} className="hidden group-hover:flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
                                 {navlinkProps.dropdownItem1Icon && (
                                     <Image src={navlinkProps.dropdownItem1Icon} alt="Dropdown menu icon." width={30} />
                                 )}
@@ -54,7 +54,7 @@ function NavLink(navlinkProps: navLinkProps) {
                             </Link>
                         )}
                         {navlinkProps.dropdownItem2 && (
-                            <Link href={`${navlinkProps.dropdownItem2}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                            <Link href={`${navlinkProps.dropdownItem2}`} className="hidden group-hover:flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
                                 {navlinkProps.dropdownItem2Icon && (
                                     <Image src={navlinkProps.dropdownItem2Icon} alt="Dropdown menu icon." width={30} />
                                 )}
@@ -62,7 +62,7 @@ function NavLink(navlinkProps: navLinkProps) {
                             </Link>
                         )}
                         {navlinkProps.dropdownItem3 && (
-                            <Link href={`${navlinkProps.dropdownItem3}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                            <Link href={`${navlinkProps.dropdownItem3}`} className="hidden group-hover:flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
                                 {navlinkProps.dropdownItem3Icon && (
                                     <Image src={navlinkProps.dropdownItem3Icon} alt="Dropdown menu icon." width={30} />
                                 )}
@@ -70,7 +70,7 @@ function NavLink(navlinkProps: navLinkProps) {
                             </Link>
                         )}
                         {navlinkProps.dropdownItem4 && (
-                            <Link href={`${navlinkProps.dropdownItem4}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                            <Link href={`${navlinkProps.dropdownItem4}`} className="hidden group-hover:flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
                                 {navlinkProps.dropdownItem4Icon && (
                                     <Image src={navlinkProps.dropdownItem4Icon} alt="Dropdown menu icon." width={30} />
                                 )}
@@ -78,7 +78,7 @@ function NavLink(navlinkProps: navLinkProps) {
                             </Link>
                         )}
                         {navlinkProps.dropdownItem5 && (
-                            <Link href={`${navlinkProps.dropdownItem5}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                            <Link href={`${navlinkProps.dropdownItem5}`} className="hidden group-hover:flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
                                 {navlinkProps.dropdownItem5Icon && (
                                     <Image src={navlinkProps.dropdownItem5Icon} alt="Dropdown menu icon." width={30} />
                                 )}
@@ -86,7 +86,7 @@ function NavLink(navlinkProps: navLinkProps) {
                             </Link>
                         )}
                         {navlinkProps.dropdownItem6 && (
-                            <Link href={`${navlinkProps.dropdownItem6}`} className="flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
+                            <Link href={`${navlinkProps.dropdownItem6}`} className="hidden group-hover:flex flex-row justify-start items-center gap-2 transition-all duration-500 hover:scale-105">
                                 {navlinkProps.dropdownItem6Icon && (
                                     <Image src={navlinkProps.dropdownItem6Icon} alt="Dropdown menu icon." width={30} />
                                 )}
