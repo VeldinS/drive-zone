aa97d04c1208ce0ee1b8812f4c02d096be96b4b7
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-26 14:16:17 +0200

    Home page - sell&buy section - responsiveness

--- src/components/home-sections/BuySell-section.tsx
+++ src/components/home-sections/BuySell-section.tsx
@@ -9,9 +9,9 @@ import ButtonBlack from "@/components/buttons/ButtonBlack";
 
 function BuySellSection() {
     return (
-        <section className={'w-screen h-auto px-[5%] py-16 grid grid-cols-2 gap-16 items-stretch'}>
+        <section className={'w-screen h-auto px-[5%] py-16 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-stretch'}>
 
-            <div className={'w-full h-full px-16 py-16 bg-[#E9F2FF] rounded-xl flex flex-col items-start justify-start gap-0'}>
+            <div className={'relative w-full h-full px-6 sm:px-16 py-12 sm:py-16 bg-[#E9F2FF] rounded-xl flex flex-col items-start justify-start gap-4 sm:gap-0'}>
                 <div className={'flex flex-col items-start justify-start gap-4'}>
                     <h2 className={'text-primaryBlue font-poppins font-semibold text-3xl leading-relaxed'}>Are You
                         Looking <br/> For a Car</h2>
@@ -20,11 +20,11 @@ function BuySellSection() {
                 </div>
                 <div className={'w-full flex flex-row justify-between items-center'}>
                     <ButtonBlue text={'Get Started'} link={'cars'}/>
-                    <Image src={buyCarIcon} alt={'Sell Car Icon'} width={100}/>
+                    <Image className={'absolute bottom-0 right-2 sm:right-0 sm:relative'} src={buyCarIcon} alt={'Sell Car Icon'} width={100}/>
                 </div>
             </div>
 
-            <div className={'w-full h-full px-16 py-16 bg-[#FFE9F3] rounded-xl flex flex-col items-start justify-start gap-0'}>
+            <div className={'relative w-full h-full px-6 sm:px-16 py-12 sm:py-16 bg-[#FFE9F3] rounded-xl flex flex-col items-start justify-start gap-4 sm:gap-0'}>
                 <div className={'flex flex-col items-start justify-start gap-4'}>
                     <h2 className={'text-primaryBlue font-poppins font-semibold text-3xl leading-relaxed'}>Do You Want to
                         <br/> Sell a Car</h2>
@@ -33,7 +33,7 @@ function BuySellSection() {
                 </div>
                 <div className={'w-full flex flex-row justify-between items-center'}>
                     <ButtonBlack text={'Get Started'} link={'sell'}/>
-                    <Image src={sellCarIcon} alt={'Sell Car Icon'} width={100}/>
+                    <Image className={'absolute bottom-0 right-2 sm:right-0 sm:relative'} src={sellCarIcon} alt={'Sell Car Icon'} width={100}/>
                 </div>
             </div>
 
