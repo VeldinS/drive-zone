f120482ae4a01feff6268334eb04d69bfd1df504
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-26 14:11:09 +0200

    Black Button Component - implementation

---
+++ src/components/buttons/ButtonBlack.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Link from "next/link";
+import Image from "next/image";
+
+import rightArrow from '@/assets/icons/right-arrow-wide-w.svg';
+
+
+interface ButtonProps {
+    text: string,
+    link: string
+}
+
+function ButtonBlack(buttonProps: ButtonProps) {
+    return (
+        <Link href={buttonProps.link} className="group flex flex-row justify-center items-center px-6 py-3 sm:py-4 gap-4 rounded-xl bg-black border hover:scale-105 shadow-sm hover:shadow-xl border-black  transition-all duration-500">
+            <p className={'font-poppins font-normal text-base text-white tracking-wider'}>
+                {buttonProps.text}
+            </p>
+            <Image src={rightArrow} alt={'Right arrow'} width={15}
+                   className={'group-hover:rotate-45 transition duration-500'} />
+        </Link>
+    );
+}
+
+export default ButtonBlack;
