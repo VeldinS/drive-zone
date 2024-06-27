e5a3e502fdcde9272359a0a4f66158ed6108ab22
VeldinS <veldinsalcinovic123@gmail.com>
2024-06-27 13:11:46 +0200

    Home page - types section -responsiveness & deployment

--- src/components/home-sections/Types-section.tsx
+++ src/components/home-sections/Types-section.tsx
@@ -22,17 +22,17 @@ import ModelCard from "@/components/cards/ModelCard";
 function TypesSection() {
     return (
         <section className={'w-screen h-auto px-[5%] py-16 flex flex-col justify-start items-start gap-6'}>
-            <div className={'w-full h-auto flex flex-row justify-between items-center mb-8'}>
-                <h1 className={'text-primaryBlue font-poppins font-semibold text-3xl tracking-wider'}>Browse by
+            <div className={'w-full h-auto flex flex-col md:flex-row justify-between items-center mb-0 md:mb-8'}>
+                <h1 className={'text-primaryBlue font-poppins font-semibold text-2xl sm:text-3xl tracking-wider'}>Browse by
                     Type</h1>
                 <LinkButtonBlack text={'View All'} link={'/cars'}/>
             </div>
 
-            <div className={'w-full h-[30vh] grid grid-cols-2 gap-6'}>
+            <div className={'w-full h-auto lg:h-[30vh] grid grid-cols-1 lg:grid-cols-2 gap-6'}>
                 <ModelCard image={suvImage} type={'SUV'} typeLink={'/suv'} icon={suv} />
                 <ModelCard image={sedanImage} type={'Sedan'} typeLink={'/sedan'} icon={sedan} />
             </div>
-            <div className={'w-full h-[30vh] grid grid-cols-3 gap-6'}>
+            <div className={'w-full h-auto lg:h-[30vh] grid grid-cols-1 lg:grid-cols-3 gap-6'}>
                 <ModelCard image={hatchbackImage} type={'Hatchback'} typeLink={'/hatchback'} icon={hatchback} />
                 <ModelCard image={hybridImage} type={'Hybrid'} typeLink={'/hybrid'} icon={hybrid} />
                 <ModelCard image={coupeImage} type={'Coupe'} typeLink={'/coupe'} icon={coupe} />
