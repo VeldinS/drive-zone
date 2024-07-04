e0528610774f0d1819f8d6b2fe0f184465b0ba05
VeldinS <veldinsalcinovic123@gmail.com>
2024-07-04 16:15:52 +0200

    Footer - link component implementation

---
+++ src/components/footer/FooterLink.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from "next/link";
+
+interface FooterLinkProps {
+    href: string;
+    text: string;
+}
+
+function FooterLink(LinkProps: FooterLinkProps) {
+    return (
+        <Link href={LinkProps.href} className={'group flex flex-col items-stretch justify-start hover:scale-105 transition-all duration-500'}>
+            <p className={'text-base text-gray-500 font-normal group-hover:text-white transition-all duration-500'}>{LinkProps.text}</p>
+            <div className={'w-0 h-[1px] bg-transparent group-hover:w-full bg-white transition-all duration-500'}></div>
+        </Link>
+    );
+}
+
+export default FooterLink;
